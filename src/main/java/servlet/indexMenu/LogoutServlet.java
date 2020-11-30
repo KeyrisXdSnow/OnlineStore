03d5d0b597b1dbbee60cea1721150ad6af8f7496
Keyris <KeyrisXdSnow>
2020-12-01 02:51:54 +0300

    add sign out servlet

---
+++ src/main/java/servlet/indexMenu/LogoutServlet.java
@@ -0,0 +1,20 @@
+package servlet.indexMenu;
+
+import utils.AppService;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/logout")
+public class LogoutServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+        AppService.removeLoginedUser(req.getSession());
+        resp.sendRedirect(super.getServletContext().getContextPath());
+    }
+}
